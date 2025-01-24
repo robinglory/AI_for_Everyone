0c20975356886be9fa98ef283d873e2896ed2ea7
Robin Glory <koyannaingkyawtint@gmail.com>
2025-01-24 14:06:17 +0630

    Modify Opencvv-28.py

--- opencv-28.py
+++ opencv-28.py
@@ -8,17 +8,13 @@ cam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT,height)
 cam.set(cv2.CAP_PROP_FPS, 30)
 cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc(*'MJPG'))
-
 hands = mp.solutions.hands.Hands(
     static_image_mode=False,
     max_num_hands=2,
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5
 )
-
 mpDraw = mp.solutions.drawing_utils
-
-
 while True:
     myHands = []
     ignore,  frame = cam.read()
@@ -35,17 +31,12 @@ while True:
             # cv2.circle(frame,myHand[17],35,(255,0,0),-1)
             # cv2.circle(frame,myHand[18],15,(255,0,0),-1)
             # cv2.circle(frame,myHand[19],15,(255,0,0),-1)
-
             myHands.append(myHand)
             myHands.append(myHand)
             print(myHands)
             print(" ")
-
-
-
     cv2.imshow('my WEBcam', frame)
     cv2.moveWindow('my WEBcam',0,0)
     if cv2.waitKey(1) & 0xff ==ord('q'):
         break
 cam.release()
-
