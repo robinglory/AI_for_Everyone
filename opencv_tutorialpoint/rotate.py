64f72c92ff3f639db0f46c6b6ac4655cee48edda
Robin Glory <koyannaingkyawtint@gmail.com>
2025-03-31 14:41:48 +0630

    RotateImage

--- opencv_tutorialpoint/rotate.py
+++ opencv_tutorialpoint/rotate.py
@@ -9,8 +9,9 @@ height, width = img.shape[:2]
 img = cv2.resize(img,(int(width/2),int(height/2)), interpolation=cv2.INTER_CUBIC)
 
 center = (width/2, height/2)
-mat = cv2.getRotationMatrix2D(center, 90, 1)
-rotimg = cv2.warpAffine(img, mat, (int(width/2), int(height/2)))
+mat = cv2.getRotationMatrix2D(center, 45, 1)
+rotimg = cv2.warpAffine(img, mat,(width,height))
+rotimg = cv2.resize(rotimg, (400,300),interpolation=cv2.INTER_CUBIC)
 # rotimg = cv2.resize(rotimg, (int(width/2), int(height/2)), interpolation=cv2.INTER_CUBIC)
 cv2.imshow("Original Image", img)
 cv2.imshow("Rotated Image", rotimg)
