fbb8cddfab8c76e1f61b881d8443f38f042d158c
Robin Glory <koyannaingkyawtint@gmail.com>
2025-04-01 11:28:26 +0630

    Morphological exercise

--- opencv_tutorialpoint/morphological.py
+++ opencv_tutorialpoint/morphological.py
@@ -4,8 +4,8 @@ import numpy as np
 img = cv2.imread(r"C:\Users\ASUS\Documents\Python\Programs\opencv_tutorialpoint\story_lena_lenna_1.jpg", 0)
 kernel = np.ones((5,5),np.uint8)
 
-erosion = cv2.erode(img,kernel,iterations = 1)
-dilation = cv2.dilate(img,kernel,iterations= 1)
+erosion = cv2.erode(img,kernel,iterations = 2)
+dilation = cv2.dilate(img,kernel,iterations= 2)
 
 # img = cv2.resize(img, (200,400))
 # erosion = cv2.resize(erosion, (200,400))
